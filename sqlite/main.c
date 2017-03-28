139c8d9dd25d81df26ee8971c15a0b7cb623fcee
Tiago Possato <tiago.possato@ifc-videira.edu.br>
2017-03-28 19:07:41 +0000

    Buscando Id do Ambiente e inserindo na tabela de leituras

--- sqlite/main.c
+++ sqlite/main.c
@@ -1,71 +1,79 @@
-//https://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
-#include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
-#include <string.h>
 #include <sqlite3.h>
-#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 
-void insert_central_leitura(sqlite3 *obj_conexao, int sensor) {
-    
-    char *sql;
-    
-    int sensorId = sensor;
-    /*sql = "SELECT ambiente_id FROM central_sensor WHERE idRede = %d", sensorId;
-    printf("%s", sql);*/
-    char buffer[100] = "asdfg";
+#define DEBUG
 
-    int i=1;
+int rc;
+char *zErrMsg = 0;
+int ambienteId;
 
-    int n = sprintf(buffer,"%s%d",buffer,i);
-    
-    printf("%d", n);
-    
-    
-    
+void status(char *info);
+static int callback_select(void *data, int argc, char **argv, char **azColName);
+void insert_central_leitura(sqlite3 *db, int sensor);
 
-    //*SQL_STATEMENT pode ser VETOR
-    sql = "insert into central_leitura (valor, grandeza_id, sensor_id, ambiente_id) values (25.0, 1, 10, 1);";
-
-    int status;
-    char * errplace;
-    
-    status = sqlite3_exec(obj_conexao, sql, NULL, NULL, &errplace);
-    
-    if (status != SQLITE_OK) {
-        printf("Ocorreu um erro ao executar a consulta \" %s \"\n", sql);
-        printf("Erro: \n\t %s\n", errplace);
+void status(char *info){
+    if( rc != SQLITE_OK ){
+        printf("ERRO -> %s SQL error: %s\n", info, zErrMsg);
+        sqlite3_free(zErrMsg);
     }else{
-        printf("Dados inseridos com sucesso!\n");
+        printf("SUCESSO -> %s executado no Banco de Dados!\n", info);
     }
-
-
 }
 
-int main() {
-    
-    const char *arquivo = "banco.db";
-    sqlite3 *obj_conexao;
-    
-    if (!obj_conexao) {
-        printf("%m\n");
-        return 1;
+static int callback_select(void *data, int argc, char **argv, char **azColName){
+    int i;
+    for(i=0; i<argc; i++){
+        ambienteId = atoi(argv[i] ? argv[i] : "NULL");
+        #if defined(DEBUG)
+            printf("\t%s = %d\n", azColName[i], ambienteId);
+        #endif
     }
-    
-    int status = sqlite3_open_v2(arquivo, &obj_conexao, SQLITE_OPEN_READWRITE, NULL); // Utiliza o objeto sqlite3_vfs padrão.
+    return 0;
+}
 
-    if (status != SQLITE_OK) {
-        printf("Erro ao abrir conexão: \n\t%s\n", sqlite3_errmsg(obj_conexao));
-        return 1;
-    }else{
-        printf("Conectado com sucesso!\n");
-    }
+void insert_central_leitura(sqlite3 *db, int sensor) {
     
-    insert_central_leitura(obj_conexao, 2);
+    int sensorIdRede = sensor;
+    char *sql;
+    const char* data = "";
     
-    status = sqlite3_close(obj_conexao);
+    sprintf(sql, "SELECT ambiente_id FROM central_sensor WHERE idRede = %d;", sensorIdRede);
+    rc = sqlite3_exec(db, sql, callback_select, (void*)data, &zErrMsg);
+    #if defined(DEBUG)
+        status("SELECT");
+    #endif
     
-    printf("Desconectado com sucesso!\n");
+    sprintf(sql, "INSERT INTO central_leitura (valor, grandeza_id, sensor_id, ambiente_id) "\
+        "VALUES (25.0, 1, 10, %d);", ambienteId);
+    rc = sqlite3_exec(db, sql, NULL, 0, &zErrMsg);
+    #if defined(DEBUG)
+        status("INSERT");
+    #endif
+    
+}
+
+int main(int argc, char* argv[]){
+    sqlite3 *db;
+
+    rc = sqlite3_open("banco.db", &db);
+
+    #if defined(DEBUG)
+        if( rc ){
+            printf("ERRO -> Problema na conexão com o Banco de Dados: %s\n", sqlite3_errmsg(db));
+            return(0);
+        }else{
+            printf("SUCESSO -> Conexão com o Banco de Dados realizada!\n");
+        }   
+    #endif
+    
+    insert_central_leitura(db, 30);
+   
+    sqlite3_close(db);
+    #if defined(DEBUG)
+        printf("SUCESSO -> Banco de Dados desconectado!\n");    
+    #endif
     
     return 0;
 }
