84e3fe7217210f4203cb5762c060782864e1401b
Tiago Possato <tiago.possato@yahoo.com.br>
2017-03-29 18:38:08 -0300

    lendo string com uri e colocando na estrutura É preciso validar as entradas

--- src/main.c
+++ src/main.c
@@ -10,34 +10,29 @@ idRede/tipoGrandeza/grandeza/valor
 */
 
 /*
-Função que recebe uma URI e retira o último valor
-*/
-void extraiMsg(char *uri, char *saida)
-{
-    unsigned int i = 0;
-    unsigned char j = 4;
-    for (i = 0; i < sizeof(uri); i++)
-    {
-        if (i > 4 || uri[strlen(uri) - i] == '/')
-        {
-            break;
-        }
-        saida[j--] = uri[strlen(uri) - i];
-    }
-}
-/*
  * Testando a biblioteca
  */
 int main(int argc, char **argv)
 {
+    unsigned int idRede;
+    unsigned int tipoGrandeza;
+    unsigned int grandeza;
+    float valor;
+    FilaDados *DADOS = iniciaFila();
+
     char uri[20];
+    loop:
     scanf("%s", uri);
 
     char saida[5];
     unsigned int i = 0;
     unsigned char j = 4;
-
-     memset(saida, 1, sizeof(saida));
+    unsigned char k = 0;
+    //-------------------------------------------------
+    for (k = 0; k < 5; k++)
+    {
+        saida[k] = '\r';
+    }
     for (; i < strlen(uri); i++)
     {
         if (uri[i] == '/')
@@ -46,11 +41,14 @@ int main(int argc, char **argv)
         }
         saida[j++] = uri[i];
     }
-    printf("%s\n", saida);
     i++;
     j = 4;
-    memset(saida, 1, sizeof(saida));
-
+    idRede = (unsigned int)atoi(saida);
+    //-------------------------------------------------
+    for (k = 0; k < 5; k++)
+    {
+        saida[k] = '\r';
+    }
     for (; i < strlen(uri); i++)
     {
         if (uri[i] == '/')
@@ -59,11 +57,14 @@ int main(int argc, char **argv)
         }
         saida[j++] = uri[i];
     }
-    printf("%s\n", saida);
     i++;
     j = 4;
-    memset(saida, 1, sizeof(saida));
-
+    tipoGrandeza = (unsigned int)atoi(saida);
+    //-------------------------------------------------
+    for (k = 0; k < 5; k++)
+    {
+        saida[k] = '\r';
+    }
     for (; i < strlen(uri); i++)
     {
         if (uri[i] == '/')
@@ -72,12 +73,15 @@ int main(int argc, char **argv)
         }
         saida[j++] = uri[i];
     }
-    printf("%s\n", saida);
     i++;
     j = 4;
-    memset(saida, 1, sizeof(saida));
-
-    for (; i <= strlen(uri); i++)
+    grandeza = (unsigned int)atoi(saida);
+    //-------------------------------------------------
+    for (k = 0; k < 5; k++)
+    {
+        saida[k] = '\r';
+    }
+    for (; i < strlen(uri); i++)
     {
         if (uri[i] == '/')
         {
@@ -85,30 +89,15 @@ int main(int argc, char **argv)
         }
         saida[j++] = uri[i];
     }
-    printf("%s\n", saida);
+    i++;
+    j = 4;
+    valor = atof(saida);
+    //-------------------------------------------------
 
-    return (EXIT_SUCCESS);
-
-    unsigned int idRede = 2;
-    unsigned int tipoGrandeza = 3202;
-    unsigned int grandeza = 3303;
-    float valor = 25.69;
-
-    FilaDados *DADOS = iniciaFila();
-    insereDados(idRede, tipoGrandeza, grandeza, valor, DADOS);
-
-    idRede = 2;
-    tipoGrandeza = 3202;
-    grandeza = 3304;
-    valor = 56.89;
     insereDados(idRede, tipoGrandeza, grandeza, valor, DADOS);
     imprimeFilaDados(DADOS);
-
-    Dados *tmp;
-    tmp = peekDados(DADOS);
-    mostraDados(tmp);
-    removeDados(tmp, DADOS);
-    imprimeFilaDados(DADOS);
+    memset(uri, 1, sizeof(uri));
+    goto loop;
 
     return (EXIT_SUCCESS);
 }
