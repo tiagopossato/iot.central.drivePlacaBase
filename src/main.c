a37d9428a8b48038038061785cb139a2af338622
Tiago Possato <tiago.possato@yahoo.com.br>
2017-03-29 00:05:18 -0300

    iniciando entrada de dados

--- src/main.c
+++ src/main.c
@@ -1,21 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <semaphore.h>
 #include "../lib/dados.h"
 
+/*
+Padrão da URI:
+idRede/tipoGrandeza/grandeza/valor
+*/
+
+/*
+Função que recebe uma URI e retira o último valor
+*/
+void extraiMsg(char *uri, char *saida)
+{
+    unsigned int i = 0;
+    unsigned char j = 4;
+    for (i = 0; i < sizeof(uri); i++)
+    {
+        if (i > 4 || uri[strlen(uri) - i] == '/')
+        {
+            break;
+        }
+        saida[j--] = uri[strlen(uri) - i];
+    }
+}
 /*
  * Testando a biblioteca
  */
 int main(int argc, char **argv)
 {
-    
-    FilaDados *DADOS = iniciaFila();
-    
+    char uri[20];
+    scanf("%s", uri);
+
+    char saida[5];
+    unsigned int i = 0;
+    unsigned char j = 4;
+
+     memset(saida, 1, sizeof(saida));
+    for (; i < strlen(uri); i++)
+    {
+        if (uri[i] == '/')
+        {
+            break;
+        }
+        saida[j++] = uri[i];
+    }
+    printf("%s\n", saida);
+    i++;
+    j = 4;
+    memset(saida, 1, sizeof(saida));
+
+    for (; i < strlen(uri); i++)
+    {
+        if (uri[i] == '/')
+        {
+            break;
+        }
+        saida[j++] = uri[i];
+    }
+    printf("%s\n", saida);
+    i++;
+    j = 4;
+    memset(saida, 1, sizeof(saida));
+
+    for (; i < strlen(uri); i++)
+    {
+        if (uri[i] == '/')
+        {
+            break;
+        }
+        saida[j++] = uri[i];
+    }
+    printf("%s\n", saida);
+    i++;
+    j = 4;
+    memset(saida, 1, sizeof(saida));
+
+    for (; i <= strlen(uri); i++)
+    {
+        if (uri[i] == '/')
+        {
+            break;
+        }
+        saida[j++] = uri[i];
+    }
+    printf("%s\n", saida);
+
+    return (EXIT_SUCCESS);
+
     unsigned int idRede = 2;
     unsigned int tipoGrandeza = 3202;
     unsigned int grandeza = 3303;
     float valor = 25.69;
-    
+
+    FilaDados *DADOS = iniciaFila();
     insereDados(idRede, tipoGrandeza, grandeza, valor, DADOS);
 
     idRede = 2;
@@ -24,12 +103,12 @@ int main(int argc, char **argv)
     valor = 56.89;
     insereDados(idRede, tipoGrandeza, grandeza, valor, DADOS);
     imprimeFilaDados(DADOS);
-    
+
     Dados *tmp;
     tmp = peekDados(DADOS);
     mostraDados(tmp);
-    removeDados(tmp,DADOS);
+    removeDados(tmp, DADOS);
     imprimeFilaDados(DADOS);
-   
+
     return (EXIT_SUCCESS);
 }
